4cba3861fab914f613fc1ac5814bbff49d8827de
fogelmark <108455196+fogelmark@users.noreply.github.com>
2024-07-29 21:33:59 +0200

    refactor long class names

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,20 @@ import { TodosContextProvider } from "@/contexts/todos-context-provider"
 
 export default function Home() {
   return (
-    <main data-testid="main" className="bg-acapulco-700 text-acapulco-50 flex min-h-screen flex-col items-center p-4">
-      <section className="bg-acapulco-400 transition-all mx-auto flex w-full flex-col gap-6 rounded-lg p-4 shadow-lg max-w-[500px] md:max-w-[500px]">
-      <TodosContextProvider>
-        <Heading />
-        <AddTodoForm />
-        <TodoList />
-      </TodosContextProvider>
+    <main data-testid="main" className={classes.main}>
+      <section className={classes.section}>
+        <TodosContextProvider>
+          <Heading />
+          <AddTodoForm />
+          <TodoList />
+        </TodosContextProvider>
       </section>
     </main>
   )
+}
+
+const classes = {
+  main: "bg-acapulco-700 text-acapulco-50 flex min-h-screen flex-col items-center p-4",
+  section:
+    "bg-acapulco-400 mx-auto flex w-full flex-col gap-6 rounded-lg p-4 shadow-lg max-w-[500px] md:max-w-[500px]",
 }
